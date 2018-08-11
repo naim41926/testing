89dbc8bbfc73c63628eea25b532b8be6a249d059
naim41926 <naimhopkins@gmail.com>
2018-08-11 09:00:29 -0400

    add elements of a house

---
+++ src/house.java
@@ -0,0 +1,16 @@
+
+public class house {
+	public house() {
+		doors
+		windows
+		floors
+		garage
+		bedrooms
+		sqft
+		bathroom
+		worth 
+		construction date
+		last updated date
+		property taxes
+	}
+}
